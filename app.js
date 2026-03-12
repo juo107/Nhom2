f81265f9955897e0d6f585f0335d35ca2b9ac9fe
Nguyễn Quang Vinh <nguyenvinh10720@gmail.com>
2026-03-12 13:36:33 +0700

    A: Setup route for About page

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
