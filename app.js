9098d938d7b4cf1354901fc4a3469d6bd6379e5f
Nguyễn Quang Vinh <nguyenvinh10720@gmail.com>
2026-03-12 13:17:56 +0700

    Tạo trang chủ và thêm thư mục css, images, js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (CSS, Images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
